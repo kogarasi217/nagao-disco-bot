15f03743c5db42511b02f101ba6dfb0377c6bff4
kohei_nagao <aizu.188@gmail.com>
2023-03-16 03:33:36 +0000

    [feat] can run bot

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # nagao-disco-bot
+
+# environment
+- Rust v1.68
+- shuttle.rs
+  - the env file is `Secrets.toml`
+
+# run
+`cargo shuttle run`
+
+# deploy
+`cargo shuttle deploy`
--- src/lib.rs
+++ src/lib.rs
@@ -1,56 +1,46 @@
-use log::info;
+use anyhow::anyhow;
 use serenity::async_trait;
-
+use serenity::model::channel::Message;
 use serenity::model::gateway::Ready;
-use serenity::model::prelude::*;
 use serenity::prelude::*;
-use serenity::model::prelude::interaction::Interaction::ApplicationCommand;
-use serenity::model::interactions::*;
-use shuttle_service::error::CustomError;
-use shuttle_service::SecretStore;
-use sqlx::PgPool;
+use shuttle_secrets::SecretStore;
+use tracing::{error, info};
 
 struct Bot;
 
 #[async_trait]
 impl EventHandler for Bot {
-    async fn ready(&self, ctx: Context, ready: Ready) {
-        info!("{} is connected!", ready.user.name);
-
-        let guild_id = GuildId(＜DISCORD_GUILD_ID＞);
-
-　　　　　　　　　　　　　　　　//コマンドを登録
-        let commands = GuildId::set_application_commands(&guild_id, &ctx.http, |commands| {
-            commands.create_application_command(|command| {
-                command.name("hello").description("Say hello")
-            }).create_application_command(|command| {
-                command.name("bye").description("Say Good Bye")
-            })
-        })
-        .await
-        .unwrap();
-
-        info!("{:#?}", commands);
-    }
-
-    async fn interaction_create(&self,ctx: Context,interaction: serenity::model::interactions::Interaction) {
-        if let ApplicationCommand(command) = interaction {
-            let response_content = match command.data.name.as_str() {
-                "hello" => "hello!!".to_owned(),
-                "bye" => "good bye!!".to_owned(),
-                command => unreachable!("Unknown command: {}", command),
-            };
-
-            let create_interaction_response =
-                command.create_interaction_response(&ctx.http, |response| {
-                    response
-                        .kind(InteractionResponseType::ChannelMessageWithSource)
-                        .interaction_response_data(|message| message.content(response_content))
-                });
-
-            if let Err(why) = create_interaction_response.await {
-                eprintln!("Cannot respond to slash command: {}", why);
+    async fn message(&self, ctx: Context, msg: Message) {
+        if msg.content == "!hello" {
+            if let Err(e) = msg.channel_id.say(&ctx.http, "world!").await {
+                error!("Error sending message: {:?}", e);
             }
         }
     }
+
+    async fn ready(&self, _: Context, ready: Ready) {
+        info!("{} is connected!", ready.user.name);
+    }
+}
+
+#[shuttle_service::main]
+async fn serenity(
+    #[shuttle_secrets::Secrets] secret_store: SecretStore,
+) -> shuttle_service::ShuttleSerenity {
+    // Get the discord token set in `Secrets.toml`
+    let token = if let Some(token) = secret_store.get("DISCORD_TOKEN") {
+        token
+    } else {
+        return Err(anyhow!("'DISCORD_TOKEN' was not found").into());
+    };
+
+    // Set gateway intents, which decides what events the bot will be notified about
+    let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
+
+    let client = Client::builder(&token, intents)
+        .event_handler(Bot)
+        .await
+        .expect("Err creating client");
+
+    Ok(client)
 }
